9ddfda84a0eb6d0de6145f6099703e36ddd28277
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-03-22 13:44:39 -0400

    commit tree b081ef998c8f0f19c599912b7b72c8e53da0f850

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,13 +3,14 @@ require 'pry'
 module Memorable
     module ClassMethods
 
-     def reset_all
-    self.all.clear
-  end
+        def reset_all
+        self.all.clear
+        end
 
-  def count
-    self.all.count
-  end
+        def count
+        self.all.count
+        end
+    end
 
     module InstanceMethods
         def initialize
